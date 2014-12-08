a03c1033a33122d083f8ab302afa1e562a79756b
Testy McTest <hale_chan@web.de>
2014-12-08 23:23:15 +0100

    Habe mal 2 Projekte zum Bearbeiten von XML erstellt. Das eine Projekt kann XML lediglich öffnen und darstellen, aber nicht verarbeiten. Das andere kann ein XML-File holen und interpretieren.

--- DragAndDropTest2/src/com/example/draganddroptest2/MainActivity.java
+++ DragAndDropTest2/src/com/example/draganddroptest2/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.ClipData;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
