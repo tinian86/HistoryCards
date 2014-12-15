0b522196142cb2e679bfe84b4bdc9a128e69c83b
History2014 <davoudfa@yahoo.de>
2014-12-15 20:39:15 +0100

    Klasse CardQuestion hinzugefügt!

---
+++ HistoryAlpha/src/de/historyalpha/CardQuestion.java
@@ -0,0 +1,16 @@
+package de.historyalpha;
+
+public class CardQuestion {
+	
+	Card card1 = new Card(1, "Teflon", "Wann wurde das Teflon erfunden?");
+	Card card2 = new Card(2, "Mondlandung", "Wann btrat der erste Mensch den Mond?");
+	Card card3 = new Card(3, "Issos", "Wann war die Keilerei bei Issos?");
+	Card card4 = new Card(4, "Tesla", "Wann wurde Nikola Tesla geboren?");
+	Card card5 = new Card(5, "Guernica", "Wann malte Picasso seine 'Guernica'?");
+	Card card6 = new Card(6, "Olympische Spiele", "Wann fanden die Olympischen Spiele das erste mal statt?");
+	Card card7 = new Card(7, "Nobelpreis", "Wann wurde der Nobelpreis erstmalig verliehen?");
+	Card card8 = new Card(8, "Columbus", "Wann entdeckte Christoph Columbus Amerika?");
+	Card card9 = new Card(9, "Auto", "Wann wurde das erste Auto entwickelt?");
+	Card card10 = new Card(10, "Kinder Schokolade", "Wann kam die Kinder Schokolade auf den Markt?");
+
+}
