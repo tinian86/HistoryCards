3f7926d4e20586cd8c0372c786c45dd66f6f3111
MilkaSa <al_mi_06@yahoo.de>
2014-12-17 14:05:21 +0100

    Fehlerbeseitigung

--- HistoryAlpha/gen/de/historyalpha/R.java
+++ HistoryAlpha/gen/de/historyalpha/R.java
@@ -22,13 +22,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int shape=0x7f020001;
-        public static final int shape_droptarget=0x7f020002;
+        public static final int afrikanisch_holz=0x7f020000;
+        public static final int engl1=0x7f020001;
+        public static final int engl1_web=0x7f020002;
+        public static final int german1=0x7f020003;
+        public static final int german1_web=0x7f020004;
+        public static final int gradient_blau=0x7f020005;
+        public static final int gradient_gelb=0x7f020006;
+        public static final int gradient_gruen=0x7f020007;
+        public static final int gradient_lila=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int shape=0x7f02000a;
+        public static final int shape_droptarget=0x7f02000b;
     }
     public static final class id {
         public static final int BottomLayout=0x7f080003;
-        public static final int action_settings=0x7f080016;
+        public static final int LinearLayout1=0x7f080016;
+        public static final int action_settings=0x7f080018;
         public static final int bottomButton1=0x7f08000f;
         public static final int bottomButton2=0x7f080010;
         public static final int bottomButton3=0x7f080011;
@@ -49,12 +59,14 @@ public final class R {
         public static final int gridlayout=0x7f080004;
         public static final int highscoreList=0x7f080001;
         public static final int horizontalScrollView1=0x7f080002;
+        public static final int imageButton2=0x7f080017;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int acitvity_highscore=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_menue=0x7f030002;
+        public static final int startseite=0x7f030003;
     }
     public static final class menu {
         public static final int highscore_acitvity=0x7f070000;
