3e300ff312554a8d392c7da71352a7fabccd2b6d
History2014 <davoudfa@yahoo.de>
2014-12-17 15:01:52 +0100

    nochmal!

---
+++ HistoryAlpha/src/de/historyalpha/CardViewer.java
@@ -0,0 +1,13 @@
+package de.historyalpha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardViewer extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_card_viewer);
+	}
+}
